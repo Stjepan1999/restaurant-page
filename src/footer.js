b3647d567425d34b54143fea685fae7f8219792e
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2023-11-11 10:04:02 +0100

    Create footer

---
+++ src/footer.js
@@ -0,0 +1,22 @@
+import './style.css';
+
+export default function createFooter() {
+    const footer = document.createElement("div")
+    footer.classList.add("footer")
+    footer.textContent = "Developed by " 
+
+    const githubName = document.createElement("a");
+    githubName.textContent = "Stjepan1999"
+    githubName.href = "https://github.com/Stjepan1999"
+    githubName.target = "_blank"
+
+    const githubRepository = document.createElement("a");
+    githubRepository.textContent = "Source code";
+    githubRepository.href = "https://github.com/Stjepan1999/restaurant-page"
+    githubRepository.target = "_blank"
+
+    footer.appendChild(githubName);
+    footer.appendChild(githubRepository);
+
+    return footer
+}
