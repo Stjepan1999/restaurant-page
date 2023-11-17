8ae5f4d54091163203e21774357a9d4171d1e4a4
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2023-11-17 20:16:03 +0100

    Edit contact page

--- src/contact.js
+++ src/contact.js
@@ -7,26 +7,39 @@ export default function createContact() {
     background.classList.add("background")
 
     const contactDiv = document.createElement("div");
-    contactDiv.classList.add("about-page")
+    contactDiv.classList.add("contact-page")
 
     const contactHeading = document.createElement("h1");
     contactHeading.textContent = "CONTACT US";
     contactHeading.classList.add("menu-heading")
 
+    const workingTimeDiv = document.createElement("div")
+    const contactInfoDiv = document.createElement("div")
+
+    const workingTime = `
+    <h2>WORKING TIME:</h2> 
+    <p>Monday: 11:00 - 23:00</p>
+    <p>Thursday: 11:00 - 23:00</p>
+    <p>Wednesday: 11:00 - 23:00</p>
+    <p>Thursday: 11:00 - 23:00</p>
+    <p>Friday: 11:00 - 00:00</p>
+    <p>Saturday: 11:00 - 00:00</p>
+    <p>Sunday: 11:00 - 00:00</p>`
+
     const contactInfo = `
-    <h4>WORKING TIME:</h4> 
-    <p>Monday - Thursday: 11:00 - 23:00</p>
-    <p>Friday - Sunday : 11:00 - 00:00</p>
-
-    <h4>PHONE:</4>
-    <p>+12 345 6789 </p>
-
-    <h4>LOCATION:</h4>
-    <p>Bamboo Lane 35</p>`
+    <h2>LOCATION:</h2> 
+    <p>Bamboo Lane 35</p>
+    <h2>PHONE:</h2> 
+    <p>+12 345 6789</p>
+    <h2>EMAIL:</h2> 
+    <p>sakura@info.com</p>`
 
 
-    contactDiv.appendChild(contactHeading)
-    contactDiv.innerHTML += contactInfo
+    background.appendChild(contactHeading)
+    workingTimeDiv.innerHTML += workingTime
+    contactInfoDiv.innerHTML += contactInfo
+    contactDiv.appendChild(workingTimeDiv)
+    contactDiv.appendChild(contactInfoDiv)
     background.appendChild(contactDiv)
     content.appendChild(background)
 
